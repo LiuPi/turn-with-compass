aee120791c986bed7b7770584dccd0e4a99c4b55
Liudmila <elekelka@gmail.com>
2022-01-09 18:28:21 +0200

    Updated parameters check

--- compass.py
+++ compass.py
@@ -1,10 +1,10 @@
 def direction(facing, turn):
     points = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
     
-    if not (facing in points):
-        return 'Irrelevant facing parameter!'
-    if (turn % 45 > 0):
-        return 'Irrelevant turn parameter!'
+    if facing not in points:
+        return 'Invalid direction parameter!'
+    if turn % 45 > 0:
+        return 'Invalid turn parameter!'
 
     index = points.index(facing)
     position = (index + int(turn / 45)) % len(points)
